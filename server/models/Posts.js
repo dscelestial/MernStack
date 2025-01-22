541eba4c79d4cf2bef22c30a2c8dbb994f260c94
Daniel Celestial <celestial.danielstephen@gmail.com>
2025-01-22 15:27:12 +0800

    second commit

---
+++ server/models/Posts.js
@@ -0,0 +1,20 @@
+export default (sequelize, DataTypes) => {
+
+    const Posts = sequelize.define("Posts", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        postText: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+    })
+
+    return Posts
+}
